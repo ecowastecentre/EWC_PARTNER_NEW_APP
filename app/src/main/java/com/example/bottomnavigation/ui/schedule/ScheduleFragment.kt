553d1a2d7bd9a7df4b6e3ca4eb92477bcdd24c7b
arjumand <iamthinkingbee@gmail.com>
2020-12-17 23:22:20 +0530

    renamed dashboard to schedule

--- app/src/main/java/com/example/bottomnavigation/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/schedule/ScheduleFragment.kt
@@ -1,23 +1,19 @@
-package com.example.bottomnavigation.ui.dashboard
+package com.example.bottomnavigation.ui.schedule
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.bottomnavigation.NewPickUpDetail
 import com.example.bottomnavigation.R
 import com.example.bottomnavigation.SchedulePickupDetail
-import kotlinx.android.synthetic.main.fragment_newpickup.*
 
-class DashboardFragment : Fragment() {
+class ScheduleFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var scheduleViewModel: ScheduleViewModel
     private lateinit var base_cardviewschedule1: CardView
 
     override fun onCreateView(
@@ -25,9 +21,9 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        scheduleViewModel =
+            ViewModelProviders.of(this).get(ScheduleViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_schedule, container, false)
       /*  val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
--- app/src/main/java/com/example/bottomnavigation/ui/schedule/ScheduleViewModel.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/schedule/ScheduleViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.bottomnavigation.ui.dashboard
+package com.example.bottomnavigation.ui.schedule
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ScheduleViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is schedule Fragment"
     }
     val text: LiveData<String> = _text
 }
